799ce04ecd0a8c105af0fa4a0ca8ce154188fcdf
Яроцкий Алексей Влдаимирович <aleksey.yarozkiy@yandex.ru>
2022-06-25 14:52:21 +0200

    Correction

--- src/main/java/ru/netology/StatsService.java
+++ src/main/java/ru/netology/StatsService.java
@@ -1,35 +1,60 @@
 package ru.netology;
 
 public class StatsService {
-    public long sum(long [] sales){
+    public long sum(long[] sales) {  //Сумма всех продаж средняя
         long a = 0;
-        for(long sale : sales){
+        for (long sale : sales) {
             a += sale;
         }
         return a;
     }
 
-    public long average (long[] sales) {
+    public long average(long[] sales) { //Сумма продаж в месяц
         long sum = sum(sales);
         return sum / 12;
     }
-    public int maxSale (long[] sales) {
+
+    public int maxSale(long[] sales) { //месяц с максиалььным оборотом
         int maxi = 0;
-        for (int i = 0; i < sales.length; i++){
-            if (sales[i] >= sales[maxi]){
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxi]) {
                 maxi = i;
             }
         }
         return maxi;
     }
-    public int aboveAverage(long[] sales) {
-        int count =0;
-        long avg = average(sales);
+
+    public int minSales(long[] sales) { //Месяц с минимальным оборотом
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
         for (long sale : sales) {
-            if (sale > avg) {
-                count++;
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public long lowerAverage(long[] sales) {  //Месяцы в которых продажи были ниже среднего
+        long a = average(sales);
+        int numberMonth = 0;
+        for (long sale : sales) {
+            if (sale < a) {
+                numberMonth++; //счетчик
             }
         }
-        return count;
+        return numberMonth;
+    }
+
+    public long upperAverage(long[] sales) {  //Месяцы в которых продажи были выше среднего
+        long a = average(sales);
+        int numberMonth = 0;
+        for (long sale : sales) {
+            if (sale > a) {
+                numberMonth++;
+            }
+        }
+        return numberMonth;
     }
 }
--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -5,25 +5,70 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatsServiceTest {
 
-@Test
-    public void summ(){
-    StatsService service = new StatsService();
-    long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-    long actual = service.sum(sales);
-    long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
-    assertEquals (expected, actual);
+    @Test
+    public void summ() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+        assertEquals(expected, actual);
 
-}
-@Test
+    }
+    @Test
+    void countAverageSale() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
 
-public void findMaxMouth () {
-    StatsService service = new StatsService();
-    long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.average(sales);
 
-    long actual = service.maxSale(sales);
-    long expected = 7;
+        assertEquals(expected, actual);
+    }
+    @Test
 
-    assertEquals(expected, actual);
+    public void findMaxMouth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
-}
+        long actual = service.maxSale(sales);
+        long expected = 7;
+
+        assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+
+    public void findMinMouth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.minSales(sales);
+        long expected = 9;
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void lowerAverage() {
+
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.lowerAverage(sales);
+        long expected = 5;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void upperAverage() {
+
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.upperAverage(sales);
+        long expected = 5;
+        assertEquals(expected, actual);
+    }
+
 }
